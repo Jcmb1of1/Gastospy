8f2cc1bf07fa2208289d93c8abc053865aa421ac
Julio Bitencourt <jujujulio345soulegal@gmail.com>
2026-08-18 08:59:23 -0300

    First commit: Adicionando os pacotes

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+Transporte = Alimentacao = Lazer = Contas_fix = False
+tpd = ad = ld = []
+
+Sal = float(input('Digite seu sálario: '))
+while True:
+
+
