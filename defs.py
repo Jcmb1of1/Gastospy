9aae0b70d5ed01c852a6e1eabc652bdf34140bd3
Julio Bitencourt <jujujulio345soulegal@gmail.com>
2026-08-18 19:51:05 -0300

    Second commit: Adicionado novas variáveis, novas opções, e bibliotecas removidas e adicionadas

---
+++ defs.py
@@ -0,0 +1,4 @@
+import os
+
+def limpador_de_tela():
+    os.system('cls')
--- main.py
+++ main.py
@@ -1,41 +1,62 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import datetime
-
+from datetime import date
+from time import sleep as s
+from defs import limpador_de_tela
 
 #todas as variáveis constantes do projeto:
 
 sal = float(input('Salário:\n'))
+limpador_de_tela()
 
-gf = {
+ali = tp = lz = gt = 0
 
+g = {
+    'Transporte': tp,
+    'Aliimentação': ali,
+    'Lazer': lz,
+    'Gastos totais': gt
 }
-#gastos fixos
-
-
-ali, tp, lz, gt = [], [], [], []
-#alimentação, transporte, lazer, gastos totais
-
 
 while True:
+    limpador_de_tela()
+    print(date.today())
     #tela inicial
     print('GERENCIADOR DE GASTOS SIMPLES')
 
 
     #escolha do usuário - o que ele quer fazer
-    user = int(input('[1]Adicionar gastos\n[2]Ver gastos\n[3] Sair do sistema'))
+    user = int(input('[1]Adicionar gastos\n[2]Ver gastos\n[3]Sair do sistema'))
 
 
     #escolha do usuário - que gasto ele quer adicionar
     if user == 1:
+        limpador_de_tela()
         print('Qual tipo de gasto?')
         tipo = int(input('[1]Transporte\n[2]Alimentação\n[3]Lazer\n[4]Gastos Fixos'))
         if tipo == 1:
+            limpador_de_tela()
             qtgast = float(input('Insira a quantidade gasta:\n'))
-            tp.append(qtgast)
-            gt.append(qtgast)
+            tp += qtgast
+            gt += qtgast
         elif tipo == 2:
+            limpador_de_tela()
             qtgast = float(input('Insira a quantidade gasta:\n'))
-            ali.append(qtgast)
-            gt.append(qtgast)
+            gt += qtgast
+            ali += qtgast
+        elif tipo == 3:
+            limpador_de_tela()
+            qtgast = float(input('Insira a quantidade gasta:\n'))
+            gt += qtgast
+            lz += qtgast
+        elif tipo == 4:
+            nomegasto = str(input('Insira o nome da despesa:\n')).title()
+            g[nomegasto] = sum([float(input('Insira a quantidade gasta:\n'))])
+    elif user == 2:
+        #ainda pensando como configurar matplotlib
+        print()
+    elif user == 3:
+        print('Saindo...')
+        s(1)
+        break
+
 
