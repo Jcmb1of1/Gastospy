0fb0fbcb998ff79e232febed56745bb95d9da9ff
Julio Bitencourt <jujujulio345soulegal@gmail.com>
2026-08-19 20:37:12 -0300

    Fifth commit: Variáveis inutilizadas removidas, novas features, novas funções, retorno do numpy para facilitar o cálculo das porcentagens

--- defs.py
+++ defs.py
@@ -1,6 +1,7 @@
-import os
+
 
 def limpador_de_tela():
+    import os
     os.system('cls')
 
 
@@ -12,3 +13,23 @@ def continuador():
         else:
             print('Digite apenas S ou N.')
     return esc
+
+def adicionar(msg, var, saldef):
+    a = float(input(msg))
+    if a > saldef:
+        print('Erro. Valor > Salário')
+    else:
+        var += a
+        print('Adicionado com sucesso')
+    return var
+
+
+def tela_inicial():
+    from datetime import date
+    print('-' * 30)
+    print(date.today())
+    print('-' * 30)
+    print('GERENCIADOR DE GASTOS SIMPLES')
+    print('-' * 30)
+
+
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import matplotlib.pyplot as plt
-from datetime import date
 from time import sleep as s
-from defs import limpador_de_tela, continuador
+from defs import limpador_de_tela, continuador, adicionar, tela_inicial
+import numpy as np
 
 #as variáveis constantes do projeto:
 
 sal = float(input('Salário:\n'))
 limpador_de_tela()
 
-ali = tp = lz = gt = agua = luz = escola = internet = 0
+ali = tp = lz = agua = luz = escola = internet = 0
 
 g = {
     'Transporte': tp,
     'Aliimentação': ali,
     'Lazer': lz,
-    'Gastos totais': gt,
     'Conta de luz': luz,
     'Conta de internet': internet,
     'Conta de água': agua,
@@ -23,56 +22,87 @@ g = {
 
 while True:
     limpador_de_tela()
-    print(date.today())
-    #tela inicial
-    print('GERENCIADOR DE GASTOS SIMPLES')
+    tela_inicial()
 
 
     #escolha do usuário - o que ele quer fazer
-    user = int(input('[1]Adicionar gastos\n[2]Ver gastos\n[3]Sair do sistema\n'))
+    user = int(input('[1]Adicionar gastos\n[2]Ver gastos\n[3]Alterar salário\n[4]Sair do sistema\n'))
 
 
     #escolha do usuário - que gasto ele quer adicionar
     if user == 1:
+
         limpador_de_tela()
         print('Qual tipo de gasto?')
-        tipo = int(input('[1]Transporte\n[2]Alimentação\n'))
+
+        # tipos de despesas
+        tipo = int(input('[1]Transporte\n[2]Alimentação[3]Lazer\n[4]Conta de Luz\n[5]Conta de água\n[6]Mensalidade escolar\n[7]Internet'))
+
         if tipo == 1:
             limpador_de_tela()
-            qtgast = float(input('Insira a quantidade gasta:\n'))
-            tp += qtgast
-            gt += qtgast
+            tp = adicionar('Insira a quantidade gasta\n', tp, sal)
             esc = continuador()
             if esc == 'n':
                 break
         elif tipo == 2:
             limpador_de_tela()
-            qtgast = float(input('Insira a quantidade gasta:\n'))
-            gt += qtgast
-            ali += qtgast
+            ali = adicionar('Insira a quantidade gasta\n', ali, sal)
             esc = continuador()
             if esc == 'n':
                 break
         elif tipo == 3:
             limpador_de_tela()
-            qtgast = float(input('Insira a quantidade gasta:\n'))
-            gt += qtgast
-            lz += qtgast
+            lz = adicionar('Insira a quantidade gasta\n', lz, sal)
             esc = continuador()
             if esc == 'n':
                 break
-
+        elif tipo == 4:
+            limpador_de_tela()
+            agua = adicionar('Insira a quantidade gasta\n', agua, sal)
+            esc = continuador()
+            if esc == 'n':
+                break
+        elif tipo == 5:
+            limpador_de_tela()
+            luz = adicionar('Insira a quantida gasta\n', luz, sal)
+            esc = continuador()
+            if esc == 'n':
+                break
+        elif tipo == 6:
+            limpador_de_tela()
+            escola = adicionar('Insira a quantidade gasta\n', escola, sal)
+            esc = continuador()
+            if esc == 'n':
+                break
+        elif tipo == 7:
+            limpador_de_tela()
+            internet = adicionar('Insira a quantida gasta\n', internet, sal)
+            esc = continuador()
+            if esc == 'n':
+                break
+    #Opc mostra gráfico
     elif user == 2:
-        #ainda pensando como configurar matplotlib
-        porcentagens = [sal/sal * 100, tp/sal * 100, ali/sal * 100, lz/sal * 100, agua/sal*100, escola/sal*100, luz/sal*100, internet/sal*100 ]
+        array = np.array([sal, tp, ali, lz, agua, escola, luz, internet])
+        porcentagens = array/sal * 100
         nomes = ['Salário', 'Transporte', 'Alimentação', 'Lazer', 'Conta de água', 'Escola/Faculdade', 'Conta de luz', 'Conta de internet']
         plt.pie( porcentagens, labels=porcentagens)
         plt.legend(labels=nomes, loc='upper left', bbox_to_anchor=(-0.4, 1.1))
+        plt.title('Porcentagem de gastos', loc='center')
         plt.show()
-        print()
+        break
     elif user == 3:
+        sal = float(input('Novo salário:\n'))
+        esc = continuador()
+        if esc == 'n':
+            break
+    #Opc que termina a execução do programa.
+    elif user == 4:
+        limpador_de_tela()
         print('Saindo...')
         s(1)
         break
+    else:
+        limpador_de_tela()
+        print('Opção Invalida')
 
 
